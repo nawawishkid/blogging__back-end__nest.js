c1fc93173ae82df8f91722224d13911f5fbda604
nawawishkid <nawawish.samerpark@gmail.com>
2020-11-21 21:06:24 +0700

    chore: add debug logging

--- src/users/user.decorator.ts
+++ src/users/user.decorator.ts
@@ -1,7 +1,11 @@
+import debugPkg from 'debug';
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+
+const debug = debugPkg('blogging:module:users:decorator');
 
 export const User = createParamDecorator(
   (data: unknown, ctx: ExecutionContext) => {
+    debug('get user from request object');
     const request = ctx.switchToHttp().getRequest();
 
     return request.user;
