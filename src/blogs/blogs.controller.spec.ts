4867468426af384f1cbc5fd5cd671dff7f52897d
nawawishkid <nawawish.samerpark@gmail.com>
2020-11-22 18:59:03 +0700

    test: add controller & service test cases

--- src/blogs/blogs.controller.spec.ts
+++ src/blogs/blogs.controller.spec.ts
@@ -1,20 +1,155 @@
+import {
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
+import { User } from 'src/users/entities/user.entity';
 import { BlogsController } from './blogs.controller';
 import { BlogsService } from './blogs.service';
+import { CreateBlogDto } from './dto/create-blog.dto';
+import { UpdateBlogDto } from './dto/update-blog.dto';
+import { Blog } from './entities/blog.entity';
 
 describe('BlogsController', () => {
-  let controller: BlogsController;
+  let controller: BlogsController, blogsService: BlogsService;
 
   beforeEach(async () => {
+    jest.restoreAllMocks();
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [BlogsController],
-      providers: [BlogsService],
+      providers: [
+        {
+          provide: BlogsService,
+          useValue: {
+            findByAuthorId: jest.fn(),
+            remove: jest.fn(),
+            create: jest.fn(),
+            update: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<BlogsController>(BlogsController);
+    blogsService = module.get<BlogsService>(BlogsService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe(`findAll(user: User)`, () => {
+    it(`should return all blogs of given user object`, () => {
+      const foundBlogs = [{ id: '1' }, { id: '2' }] as Blog[];
+
+      jest.spyOn(blogsService, 'findByAuthorId').mockResolvedValue(foundBlogs);
+
+      return expect(controller.findAll({} as User)).resolves.toBe({
+        blogs: foundBlogs,
+      });
+    });
+
+    it(`should throw NotFoundException if the given user has no blogs`, () => {
+      jest.spyOn(blogsService, 'findByAuthorId').mockResolvedValue(undefined);
+
+      return expect(controller.findAll({} as User)).rejects.toThrow(
+        NotFoundException,
+      );
+    });
+  });
+
+  describe(`findOne(blogId: string)`, () => {
+    it(`should return the blog with the given id`, () => {
+      const foundBlog = {} as Blog;
+
+      jest.spyOn(blogsService, 'findOne').mockResolvedValue(foundBlog);
+
+      return expect(controller.findOne('id')).resolves.toBe(foundBlog);
+    });
+
+    it(`should throw NotFoundException if the blog with the given id could not be found`, () => {
+      jest.spyOn(blogsService, 'findOne').mockResolvedValue(undefined);
+
+      return expect(controller.findOne('not-found')).rejects.toThrow(
+        NotFoundException,
+      );
+    });
+  });
+
+  describe(`create(user: User, createBlogDto: CreateBlogDto)`, () => {
+    it(`should return created blog`, () => {
+      const createdBlog = {} as Blog;
+
+      jest.spyOn(blogsService, 'create').mockResolvedValue(createdBlog);
+
+      return expect(
+        controller.create({} as User, {} as CreateBlogDto),
+      ).resolves.toBe(createdBlog);
+    });
+
+    it(`should throw the InternalServerErrorException if there is an error thrown by blogsService`, () => {
+      jest.spyOn(blogsService, 'create').mockRejectedValueOnce(new Error());
+
+      return expect(
+        controller.create({} as User, {} as CreateBlogDto),
+      ).rejects.toThrow(InternalServerErrorException);
+    });
+  });
+
+  describe(`update(blogId: string, updateBlogDto: UpdateBlogDto)`, () => {
+    it(`should return updated blog`, () => {
+      const updatedBlog = {} as Blog;
+
+      jest.spyOn(blogsService, 'update').mockResolvedValue(updatedBlog);
+
+      return expect(
+        controller.update('blog-id', {} as UpdateBlogDto),
+      ).resolves.toBe(updatedBlog);
+    });
+
+    it(`should throw the NotFoundException if the blog with the given id could not be found`, () => {
+      jest
+        .spyOn(blogsService, 'update')
+        .mockRejectedValue(new BlogNotFoundException());
+
+      return expect(
+        controller.update('blog-id', {} as UpdateBlogDto),
+      ).rejects.toThrow(NotFoundException);
+    });
+
+    it(`should throw the InternalServerErrorException if there is another error thrown by blogsService`, () => {
+      jest.spyOn(blogsService, 'update').mockRejectedValue(new Error());
+
+      return expect(
+        controller.update('blog-id', {} as UpdateBlogDto),
+      ).rejects.toThrow(InternalServerErrorException);
+    });
+  });
+
+  describe(`remove(blogId: string)`, () => {
+    it(`should return nothing if successfully remove`, () => {
+      jest.spyOn(blogsService, 'remove').mockResolvedValue('blog-id');
+
+      return expect(controller.remove('blog-id')).resolves.toBeUndefined();
+    });
+
+    it(`should throw the NotFoundException if the blog with the given id could not be found`, () => {
+      jest
+        .spyOn(blogsService, 'remove')
+        .mockRejectedValue(new BlogNotFoundException());
+
+      return expect(controller.remove('blog-id')).rejects.toThrow(
+        NotFoundException,
+      );
+    });
+
+    it(`should throw the InternalServerErrorException if there is another error thrown by the blogsService`, () => {
+      jest.spyOn(blogsService, 'remove').mockRejectedValue(new Error());
+
+      return expect(controller.remove('blog-id')).rejects.toThrow(
+        InternalServerErrorException,
+      );
+    });
   });
 });
--- src/blogs/blogs.service.spec.ts
+++ src/blogs/blogs.service.spec.ts
@@ -1,18 +1,142 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { DeleteResult, Repository } from 'typeorm';
 import { BlogsService } from './blogs.service';
+import { CreateBlogDto } from './dto/create-blog.dto';
+import { UpdateBlogDto } from './dto/update-blog.dto';
+import { Blog } from './entities/blog.entity';
 
 describe('BlogsService', () => {
-  let service: BlogsService;
+  let service: BlogsService, blogsRepository: Repository<Blog>;
 
   beforeEach(async () => {
+    jest.restoreAllMocks();
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [BlogsService],
+      providers: [
+        BlogsService,
+        {
+          provide: getRepositoryToken(Blog),
+          useValue: { find: jest.fn(), save: jest.fn() },
+        },
+      ],
     }).compile();
 
     service = module.get<BlogsService>(BlogsService);
+    blogsRepository = module.get<Repository<Blog>>(getRepositoryToken(Blog));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe(`findByAuthorId(userId: number)`, () => {
+    it(`should return all blogs of the user with given id`, async () => {
+      const authorId = 1;
+      let receivedAuthorId;
+
+      jest
+        .spyOn(blogsRepository, 'find')
+        .mockImplementation((condition: any) => {
+          receivedAuthorId = condition.where.author;
+
+          return Promise.resolve([
+            { id: 'blog-1' },
+            { id: 'blog-2' },
+          ] as Blog[]);
+        });
+
+      await expect(service.findByAuthorId(authorId)).resolves.toBe(
+        blogsRepository.find({ where: { author: authorId } }),
+      );
+      expect(receivedAuthorId).toEqual(authorId);
+    });
+
+    it(`should return undefined if the user has no blog`, () => {
+      jest.spyOn(blogsRepository, 'find').mockResolvedValue(undefined);
+
+      return expect(service.findByAuthorId(1)).resolves.toBeUndefined();
+    });
+  });
+
+  describe(`findOne(blogId: string)`, () => {
+    it(`should return a blog with given id`, () => {
+      const foundBlog = {} as Blog;
+
+      jest.spyOn(blogsRepository, 'findOne').mockResolvedValue(foundBlog);
+
+      return expect(service.findOne('blog-1')).resolves.toBe(foundBlog);
+    });
+
+    it(`should return undefined if a blog with given id could not be found`, () => {
+      jest.spyOn(blogsRepository, 'findOne').mockResolvedValue(undefined);
+
+      return expect(service.findOne('blog-1')).resolves.toBeUndefined();
+    });
+  });
+
+  describe(`create(createBlogDto: CreateBlogDto)`, () => {
+    it(`should return created blog`, () => {
+      const createBlogDto = {} as CreateBlogDto;
+      const createdBlog = {} as Blog;
+
+      jest.spyOn(blogsRepository, 'save').mockResolvedValue(createdBlog);
+
+      return expect(service.create(createBlogDto)).resolves.toBe(createdBlog);
+    });
+
+    it(`should throw what blogs repository throws`, () => {
+      const createBlogDto = {} as CreateBlogDto;
+      const error = new Error();
+
+      jest.spyOn(blogsRepository, 'save').mockRejectedValue(error);
+
+      return expect(service.create(createBlogDto)).rejects.toThrow(error);
+    });
+  });
+
+  describe(`update(updateBlogDto: UpdateBlogDto)`, () => {
+    it(`should return updated blog`, () => {
+      const updateBlogDto = {} as UpdateBlogDto;
+      const updatedBlog = {} as Blog;
+
+      jest.spyOn(blogsRepository, 'save').mockResolvedValue(updatedBlog);
+
+      return expect(service.update('blog-id', updateBlogDto)).resolves.toBe(
+        updatedBlog,
+      );
+    });
+
+    it(`should throw the BlogNotFoundException if given blog id could not be found (no upsert)`, () => {
+      jest
+        .spyOn(blogsRepository, 'save')
+        .mockRejectedValue(new BlogNotFoundException());
+
+      return expect(
+        service.update('blog-not-found', {} as UpdateBlogDto),
+      ).rejects.toThrow(BlogNotFoundException);
+    });
+  });
+
+  describe(`remove(blogId: string)`, () => {
+    it(`should return removed blog id`, () => {
+      const blogId = 'blog-id';
+
+      jest
+        .spyOn(blogsRepository, 'delete')
+        .mockResolvedValue({ affected: 0 } as DeleteResult);
+
+      return expect(service.remove(blogId)).resolves.toBe(blogId);
+    });
+
+    it(`should throw BlogNotFoundException if given blog id could not be found`, () => {
+      jest
+        .spyOn(blogsRepository, 'delete')
+        .mockResolvedValue({ affected: null } as DeleteResult);
+
+      return expect(service.remove('hahaha')).rejects.toThrow(
+        BlogNotFoundException,
+      );
+    });
   });
 });
