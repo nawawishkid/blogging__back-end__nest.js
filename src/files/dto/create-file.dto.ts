3f42fb01bbbe8d3cb30f19fc1be9f25be88e3017
nawawishkid <nawawish.samerpark@gmail.com>
2020-11-23 14:15:40 +0700

    progress: update create & update file dtos

--- src/files/dto/create-file.dto.ts
+++ src/files/dto/create-file.dto.ts
@@ -1,6 +1,19 @@
-import { IsString } from 'class-validator';
+import { IsInt, IsObject, IsString, IsUrl } from 'class-validator';
 
+export class FileObject {
+  @IsUrl()
+  path: string;
+
+  @IsInt()
+  size: number;
+
+  @IsString()
+  type: string;
+}
 export class CreateFileDto {
   @IsString()
   name: string;
+
+  @IsObject()
+  file: FileObject;
 }
