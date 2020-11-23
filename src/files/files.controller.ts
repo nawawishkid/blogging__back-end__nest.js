0827711b27e296d9c5875667399ce1ad1dd8a321
nawawishkid <nawawish.samerpark@gmail.com>
2020-11-23 14:33:19 +0700

    progress: define files controller functionalities

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,34 +1,86 @@
-import { Controller, Get, Post, Body, Put, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Put,
+  Param,
+  Delete,
+  HttpCode,
+  NotFoundException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { FilesService } from './files.service';
 import { CreateFileDto } from './dto/create-file.dto';
 import { UpdateFileDto } from './dto/update-file.dto';
+import { File } from './entities/file.entity';
+import { FileNotFoundException } from './exceptions/file-not-found.exception';
+import {
+  CreateFileResponseDto,
+  FindAllFilesResponseDto,
+  FindOneFileResponseDto,
+  UpdateFileResponseDto,
+} from './dto/response.dto';
 
 @Controller('files')
 export class FilesController {
   constructor(private readonly filesService: FilesService) {}
 
   @Post()
-  create(@Body() createFileDto: CreateFileDto) {
-    return this.filesService.create(createFileDto);
+  async create(
+    @Body() createFileDto: CreateFileDto,
+  ): Promise<CreateFileResponseDto> {
+    const createdFile: File = await this.filesService.create(createFileDto);
+
+    return { createdFile };
   }
 
   @Get()
-  findAll() {
-    return this.filesService.findAll();
+  async findAll(): Promise<FindAllFilesResponseDto> {
+    const files: File[] = await this.filesService.findAll();
+
+    if (!files) throw new NotFoundException();
+
+    return { files };
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.filesService.findOne(+id);
+  async findOne(@Param('id') id: string): Promise<FindOneFileResponseDto> {
+    const file: File = await this.filesService.findOne(+id);
+
+    if (!file) throw new NotFoundException();
+
+    return { file };
   }
 
   @Put(':id')
-  update(@Param('id') id: string, @Body() updateFileDto: UpdateFileDto) {
-    return this.filesService.update(+id, updateFileDto);
+  async update(
+    @Param('id') id: string,
+    @Body() updateFileDto: UpdateFileDto,
+  ): Promise<UpdateFileResponseDto> {
+    try {
+      const updatedFile: File = await this.filesService.update(
+        +id,
+        updateFileDto,
+      );
+
+      return { updatedFile };
+    } catch (e) {
+      if (e instanceof FileNotFoundException) throw new NotFoundException();
+
+      throw new InternalServerErrorException();
+    }
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.filesService.remove(+id);
+  @HttpCode(204)
+  async remove(@Param('id') id: string): Promise<void> {
+    try {
+      await this.filesService.remove(+id);
+    } catch (e) {
+      if (e instanceof FileNotFoundException) throw new NotFoundException();
+
+      throw new InternalServerErrorException();
+    }
   }
 }
