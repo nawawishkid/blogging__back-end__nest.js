41eb763d3c2f728501fcd2812f26ff21d2432038
nawawishkid <nawawish.samerpark@gmail.com>
2020-11-23 09:53:39 +0700

    progress: add create blog dto

--- src/blogs/dto/create-blog.dto.ts
+++ src/blogs/dto/create-blog.dto.ts
@@ -1,1 +1,17 @@
-export class CreateBlogDto {}
+import { IsOptional, IsString } from 'class-validator';
+
+export class CreateBlogDto {
+  @IsString()
+  title: string;
+
+  @IsString()
+  body: string;
+
+  @IsOptional()
+  @IsString()
+  coverImage?: string;
+
+  @IsOptional()
+  @IsString()
+  excerpt?: string;
+}
