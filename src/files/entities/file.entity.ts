111748ea48ad8a01f52337fb98974b0429d4c080
nawawishkid <nawawish.samerpark@gmail.com>
2020-11-23 13:40:47 +0700

    progress: add file entity

--- src/files/entities/file.entity.ts
+++ src/files/entities/file.entity.ts
@@ -1,1 +1,15 @@
-export class File {}
+import { Column, PrimaryGeneratedColumn } from 'typeorm';
+
+export class File {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column('text')
+  path: string;
+
+  @Column()
+  type: string;
+
+  @Column('int')
+  size: number;
+}
