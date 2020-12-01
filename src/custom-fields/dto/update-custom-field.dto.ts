e49a0e3e8934ff4e723de229a9c19d98077ea139
nawawishkid <nawawish.samerpark@gmail.com>
2020-12-01 16:38:38 +0700

    fix: add validation condition to only validate if the DTO is an empty object (not allow empty object)

--- src/custom-fields/dto/update-custom-field.dto.ts
+++ src/custom-fields/dto/update-custom-field.dto.ts
@@ -1,4 +1,28 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateCustomFieldDto } from './create-custom-field.dto';
+import {
+  IsArray,
+  isNotEmptyObject,
+  IsString,
+  ValidateIf,
+} from 'class-validator';
 
-export class UpdateCustomFieldDto extends PartialType(CreateCustomFieldDto) {}
+/**
+ * Not allow empty object
+ */
+const condition = o => !isNotEmptyObject(o);
+const option = {
+  message: `The DTO must not be an empty object. All fields are optional but at least 1 field is required`,
+};
+
+export class UpdateCustomFieldDto {
+  @ValidateIf(condition, option)
+  @IsString()
+  name?: string;
+
+  @ValidateIf(condition, option)
+  @IsString()
+  description?: string;
+
+  @ValidateIf(condition, option)
+  @IsArray()
+  values?: string[];
+}
