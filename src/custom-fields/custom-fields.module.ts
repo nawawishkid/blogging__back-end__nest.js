64c9972876e9bb293a9f2f3a47e5b96c5d490d4c
nawawishkid <nawawish.samerpark@gmail.com>
2020-12-01 15:28:25 +0700

    test: change import path

--- src/custom-fields/custom-fields.module.ts
+++ src/custom-fields/custom-fields.module.ts
@@ -3,7 +3,7 @@ import { CustomFieldsService } from './custom-fields.service';
 import { CustomFieldsController } from './custom-fields.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CustomField } from './entities/custom-field.entity';
-import { CustomFieldValuesModule } from 'src/custom-field-values/custom-field-values.module';
+import { CustomFieldValuesModule } from '../custom-field-values/custom-field-values.module';
 
 @Module({
   imports: [TypeOrmModule.forFeature([CustomField]), CustomFieldValuesModule],
