46a50f00d3db48fb983f36a5dbdf6bec5069c398
nawawishkid <nawawish.samerpark@gmail.com>
2020-12-01 18:01:52 +0700

    fix: remove customFieldId from the DTO

--- src/custom-field-values/dto/update-custom-field-value.dto.ts
+++ src/custom-field-values/dto/update-custom-field-value.dto.ts
@@ -1,4 +1,19 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateCustomFieldValueDto } from './create-custom-field-value.dto';
+import { isNotEmptyObject, IsString, ValidateIf } from 'class-validator';
 
-export class UpdateCustomFieldValueDto extends PartialType(CreateCustomFieldValueDto) {}
+/**
+ * Not allow empty object
+ */
+const condition = o => !isNotEmptyObject(o);
+const option = {
+  message: `The DTO must not be an empty object. All fields are optional but at least 1 field is required`,
+};
+
+export class UpdateCustomFieldValueDto {
+  @ValidateIf(condition, option)
+  @IsString()
+  value: string;
+
+  @ValidateIf(condition, option)
+  @IsString()
+  description?: string;
+}
