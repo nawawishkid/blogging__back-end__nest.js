26d7525fe56873f73125dbcab89f6a3227120ab3
nawawishkid <nawawish.samerpark@gmail.com>
2020-12-01 12:38:49 +0700

    progress: remove custom field relation from blog entity

--- src/blogs/entities/blog-custom-field.entity.ts
+++ src/blogs/entities/blog-custom-field.entity.ts
@@ -1,15 +1,11 @@
-import { CustomFieldValue } from 'src/custom-fields/entities/custom-field-value.entity';
-import { CustomField } from 'src/custom-fields/entities/custom-field.entity';
-import { Column, Entity, JoinColumn, ManyToOne, PrimaryColumn } from 'typeorm';
+import { CustomFieldValue } from '../../custom-field-values/entities/custom-field-value.entity';
+import { Entity, ManyToOne, PrimaryColumn } from 'typeorm';
 import { Blog } from './blog.entity';
 
 @Entity()
 export class BlogCustomField {
   @PrimaryColumn('uuid')
   blogId: string;
-
-  @PrimaryColumn('int')
-  customFieldId: number;
 
   @PrimaryColumn('int')
   customFieldValueId: number;
@@ -19,12 +15,6 @@ export class BlogCustomField {
     blog => blog.blogCustomFields,
   )
   blog: Blog;
-
-  @ManyToOne(
-    () => CustomField,
-    cf => cf.blogCustomFields,
-  )
-  customField: CustomField;
 
   @ManyToOne(
     () => CustomFieldValue,
